aa11da3b8ff99b2c05b4c56fe1c57ed765ae6221
juandiossa <djuan.d.o@gmail.com>
2022-07-23 22:52:55 -0500

    listo casi por completo, puede ser publicada tambien en gh-pages y se continuo el estilo con tailwindcss

--- src/assets/main.js
+++ src/assets/main.js
@@ -14,6 +14,7 @@ const month = date.toString();
 const img = document.getElementById("img");
 const section = document.querySelector("section");
 const key = "Enter";
+const errText = document.querySelector("small");
 
 async function fetchData(urlApi){
     const response = await fetch(urlApi);
@@ -23,19 +24,26 @@ async function fetchData(urlApi){
 
 async function showWeather(){
 
+    try {
+    
+        
     const API = `https://api.openweathermap.org/data/2.5/weather?appid=${KEY}&lang=${lang}&units=${units}&q=${city.value}`;
 
     const weather = await fetchData(API);
 
+    if (weather.cod == 200){
+        errText.className = "mx-3 hidden";
+    }
+
     let speed = Math.floor(((weather.wind.speed*1)/1000) * (3600/1));
 
-    section.className = "m-8 bg-gray-500 grid grid-cols-2 rounded shadow-lg p-4";
+    section.className = "my-8 bg-[#80ccff] bg-opacity-90 md:grid md:grid-cols-3 rounded-lg shadow-lg p-4 md:justify-items-stretch w-full flex flex-col";
     img.className = "block";
     img.src = `https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`;
 
     showDate.innerHTML = `${date.getDate()}/${month.substr(4,3)}/${date.getFullYear()}`;
 
-    showCity.innerHTML = weather.name|| "null";
+    showCity.innerHTML = weather.name+", "+weather.sys.country;
 
     showTemp.innerHTML = `${weather.main.temp}°C`;
 
@@ -44,6 +52,13 @@ async function showWeather(){
     showWind.innerHTML = `${speed} Km/h`;
 
     showState.innerHTML = `${weather.weather[0].description}`;
+
+    } catch (err) {
+        errText.className = "mx-3 text-red-300 italic"
+        // alert("error en ciudad/pais ingresado");
+        console.log(err);
+    }
+
 
 
 }
